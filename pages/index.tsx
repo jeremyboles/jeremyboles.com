783e5dfeaaddd7c38e9975ae760a90b316a9a1e9
Jeremy Boles <jeremy@centralstandard.com>
2021-07-09 06:18:39 -0500

    feat: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Jeremy Boles</title>
+      </Head>
+
+      <img alt="" height="24" src="/knot.svg" width="24" />
+      <h1>I am Jeremy Boles</h1>
+      <p>There’s not much to see here, for now.</p>
+      <p>
+        <a href="mailto:me@jeremyboles.com">me@jeremyboles.com</a>
+      </p>
+    </>
+  )
+}
