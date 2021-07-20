a1098a83e9147c0f39642f6b748f44fc638cf5d4
Jeremy Boles <jeremy@centralstandard.com>
2021-07-20 09:03:09 -0500

    feat: back to drawing the map manually, but at lower resolution

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -1,15 +1,21 @@
-import { geoMercator } from 'd3-geo'
+import { geoMercator, geoPath } from 'd3-geo'
 import dynamic from 'next/dynamic'
 import React from 'react'
+import { feature } from 'topojson-client'
+import json from 'world-atlas/land-110m.json'
 
 import map from './map.svg'
-
 //
 // Settings and configuration
 // -------------------------------------------------------------------------------------------------
 
 const CENTER = [0, 58]
 const ROTATE = [-11.25, 0, 0]
+
+const { features } = feature(json, json.objects[Object.keys(json.objects)[0]])
+const PROJECTION = geoMercator().center(CENTER).rotate(ROTATE)
+
+const path = geoPath().projection(PROJECTION)
 
 //
 // Typescript
@@ -29,17 +35,19 @@ interface MapProps {
 }
 
 const Map = dynamic(async () => {
-  const PROJECTION = geoMercator().center(CENTER).rotate(ROTATE)
-
-  return function Map({ points }: MapProps) {
+  return function Map({ points = [] }: MapProps) {
     return (
       <svg height="624" viewBox="0 0 960 624" width="960" xmlns="http://www.w3.org/2000/svg">
-        <use href={`${map.src}#map`} />
+        <g className="map">
+          {features.map((feature) => (
+            <path d={path(feature)} key={JSON.stringify(feature)} fill="currentColor" />
+          ))}
+        </g>
 
         <g className="points">
           {points.map(({ latitude, longitude }) => {
             const [cx, cy] = PROJECTION([longitude, latitude])
-            return <circle cx={cx} cy={cy} key={`${latitude}${longitude}`} fill="white" r="12" />
+            return <circle cx={cx} cy={cy} key={`${latitude}${longitude}`} fill="white" r="8" />
           })}
         </g>
       </svg>
